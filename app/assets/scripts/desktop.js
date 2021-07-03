3b38692d1af9d1319d0f7f7cf723cef3e90c87a5
Zheng Lai <zhenglai3@gmail.com>
2021-07-03 08:11:29 -0400

    cleaned up project files; created navbar and two column Bootstrap layout

--- app/assets/scripts/desktop.js
+++ app/assets/scripts/desktop.js
@@ -1,3 +1,3 @@
-import toggleHeader from './modules/toggleHeader';
+// import toggleHeader from './modules/toggleHeader';
 
-window.addEventListener('scroll', toggleHeader);
+// window.addEventListener('scroll', toggleHeader);
